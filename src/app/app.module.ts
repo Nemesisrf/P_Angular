faa1109b66a78869b2b241094088407ab6e6ce60
lariza azuaje <azuajelariza@gmail.com>
2021-11-25 22:54:19 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './home/login/login.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import { RegisterComponent } from './home/register/register.component';
+import { ListTaskComponent } from './board/list-task/list-task.component';
+import { SaveTaskComponent } from './board/save-task/save-task.component';
+import { ListRoleComponent } from './admin/list-role/list-role.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { RegisterRoleComponent } from './admin/register-role/register-role.component';
+import { RegisterUserComponent } from './admin/register-user/register-user.component';
+import { UpdateRoleComponent } from './admin/update-role/update-role.component';
+import { UpdateUserComponent } from './admin/update-user/update-user.component';
+
+import { UserService } from './services/user.service';
+import { RoleService } from './services/role.service';
+import { BoardService } from './services/board.service';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+
+import { AuthGuard } from './guard/auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent,
+    RegisterComponent,
+    ListTaskComponent,
+    SaveTaskComponent,
+    ListRoleComponent,
+    ListUserComponent,
+    RegisterRoleComponent,
+    RegisterUserComponent,
+    UpdateRoleComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ UserService,
+    RoleService,
+    BoardService,
+    TokenInterceptorService,
+    AuthGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
